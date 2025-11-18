a7c12ee568058fd861ad4ea7827e3a9ac865da8f
AlouiOmar97 <omar.aloui@esprit.tn>
2025-11-18 14:24:18 +0100

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello 4INFINI1 !';
+  tab = [1,2,3,4,"five"];
+  x= 4;
+  text: string = "";
+  y: boolean = true;
+  getNumbers(): number{
+    return 10;
+  }
+
+  fillText(){
+    this.text = "This is some filled text.";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [
